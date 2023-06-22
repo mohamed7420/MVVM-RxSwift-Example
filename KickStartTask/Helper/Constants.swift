d5c5a9e392babd69c8f34209b679520e47cf4151
MohamedOsama <Mohamedosama9878@gmail.com>
2023-06-22 04:57:18 +0300

    adding constants files

---
+++ KickStartTask/Helper/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  KickStartTask
+//
+//  Created by MohamedOsama on 20/06/2023.
+//
+
+import Foundation
+
+struct Constants {
+        static let apiURL = "https://run.mocky.io/v3/9677ed07-93eb-42a6-9e26-5a9cbbca658f"
+}
